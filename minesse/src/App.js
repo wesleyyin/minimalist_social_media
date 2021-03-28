fe05796c57aa9c498638c931c0a34c7e5ede6920
wesleyyin <wesleyyin@wustl.edu>
2021-03-28 09:21:15 -0500

    added some react stuff

--- minesse/src/App.js
+++ minesse/src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
-import ExercisesList from "./components/exercises-list.component";
-import EditExercises from "./components/edit-exercises.component";
-import CreateExercises from "./components/create-exercises.component";
-import CreateUsers from "./components/create-user.component";
+import Login from "./components/login.component";
+import Feed from "./components/feed.component";
+import UserProfile from "./components/user-profile.component";
+import UserSearch from "./components/user-search.component";
+import NewPost from "./components/new-post.component";
+
 function App() {
   return (
     <Router>
-      <div className = "container">
-        <Navbar />
-        <br/>
-        <Route path = "/" exact component = {ExercisesList} />
-        <Route path = "/edit/:id" exact component = {EditExercises} />
-        <Route path = "/create" exact component = {CreateExercises} />
-        <Route path = "/user" exact component = {CreateUsers} />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <div className = "container">
+          <Navbar />
+          <br/>
+          <Route path = "/feed" exact component = {Feed} />
+          <Route path = "/profiles/:username" exact component = {UserProfile} />
+          <Route path = "/search" exact component = {UserSearch} />
+          <Route path = "/new_post" exact component = {NewPost} />
       </div>
+      </Switch>
+      
       
     </Router>
   );
